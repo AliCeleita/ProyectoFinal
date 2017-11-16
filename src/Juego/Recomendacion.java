924584c044efd036e201a3f51d17b6e910c8db17
Steven Hernandez Rios <estevenhr@hotmail.com>
2017-11-15 20:44:17 -0500

    Recomendación
    Realizo algunas sugerencias que podrían ir mejorando durante el desarrollo.

---
+++ src/Juego/Recomendacion.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego;
+
+/**
+ *
+ * @author STEVEN
+ */
+public class Recomendacion {
+    /**
+     * 
+     * Importante ir documentando el codigo, no esperemos hasta lo ultimo para
+     * ir describiendo ¿de qué se encarga una clase?, ¿qué hace un método?
+     * ¿para qué se usa una variable?
+     * 
+     * Recuerden hacer Test Unatarios ha metodos como por ejemplo en la clase 
+     * Persona: static Persona crear
+     * 
+     * No vi en los campos de la base de datos fechas de registro de seguridad,
+     * el LOG de seguridad es importante en cualquier aplicación
+     * 
+     * Recuerden separar por buenas practicas la parte gráfica de la lógica, e
+     * incluso los Repositorios
+     * 
+     * Tiene que tener commits nivelados, hay muchos de un solo usuario,
+     * nivelen cargas de trabajo o funcionalidades  del sistema
+     * 
+     * Me genera un error en la Clase Registrar, creo que es por un componente 
+     * personalizado que realizaron
+     * 
+     * Deben las librerías JUnit para realizar las pruebas unitarias
+     * 
+     * Me falta la base de datos, guardarla en una carpeta para poder ejecutar
+     * el proyecto
+     * 
+     * 
+     * MUCHOS EXITOS!!!
+     * 
+     **/
+      
+}
